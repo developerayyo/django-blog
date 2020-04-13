0bafae22b45ad34c01657a82d8e297f520411823
ayobabalola-git <ayodelebabalola1004@gmail.com>
2020-04-13 14:14:42 +0100

    added taggit to models

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -13,8 +14,8 @@ class PublishedManager(models.Manager):
             .filter(status='published')
 
 
-class Post(models.Model)\
-        :
+class Post(models.Model):
+    tags = TaggableManager()
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
